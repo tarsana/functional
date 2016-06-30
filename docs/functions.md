5f3fbfacd0b35ae680f26ccd224a057d43b31815
Amine Ben hammou <webneat@gmail.com>
2016-06-30 17:09:59 +0000

    added examples to functions doc

--- docs/functions.md
+++ docs/functions.md
@@ -11,6 +11,14 @@ curry(callable $fn) : callable
 ```
 
 Returns a curried equivalent of the provided function.
+```php
+$add = curry(function($x, $y){
+    return $x + $y;
+});
+$addFive = $add(5); // a function
+$addFive(5); // 10
+$add(5, 5) // 10
+```
 
 ## __
 
@@ -22,7 +30,16 @@ __() : \Cypress\Curry\Placeholder
 * -> Placeholder
 ```
 
-Argument placeholder.
+Argument placeholder to use with curried functions.
+```php
+$minus = curry(function ($x, $y) { return $x - $y; });
+$decrement = $minus(__(), 1);
+$decrement(10) // 9
+
+$reduce = curry('array_reduce');
+$sum = $reduce(__(), 'Tarsana\Functional\plus');
+$sum([1, 2, 3, 4], 0) // 10
+```
 
 ## apply
 
@@ -35,6 +52,11 @@ apply(callable $fn, array $args) : mixed
 ```
 
 Apply the provided function to the list of arguments.
+```php
+apply('strlen', ['Hello']) // 5
+$replace = apply('str_replace');
+$replace(['l', 'o', 'Hello']) // 'Heooo'
+```
 
 ## pipe
 
@@ -50,6 +72,13 @@ Performs left-to-right function composition.
 The leftmost function may have any arity;
 the remaining functions must be unary.
 The result of pipe is curried.
+**Calling pipe() without any argument throws Tarsana\Functional\Exceptions\InvalidArgument**
+```php
+function add($x, $y) { return $x + $y; }
+$double = function($x) { return 2 * $x; };
+$addThenDouble = pipe('add', $double);
+$addThenDouble(2, 3) // 10
+```
 
 ## identity
 
@@ -63,3 +92,8 @@ identity() : callable
 
 A function that takes one argument and
 returns exactly the given argument.
+```php
+identity('Hello') // 'Hello'
+identity([1, 2, 3]) // [1, 2, 3]
+identity(null) // null
+```
--- src/functions.php
+++ src/functions.php
@@ -7,6 +7,14 @@ use Tarsana\Functional\Exceptions\InvalidArgument;
 
 /**
  * Returns a curried equivalent of the provided function.
+ * ```php
+ * $add = curry(function($x, $y){
+ *     return $x + $y;
+ * });
+ * $addFive = $add(5); // a function
+ * $addFive(5); // 10
+ * $add(5, 5) // 10
+ * ```
  *
  * @signature (* -> a) -> (* -> a)
  * @param  callable $fn
@@ -17,7 +25,16 @@ function curry(callable $fn) {
 }
 
 /**
- * Argument placeholder.
+ * Argument placeholder to use with curried functions.
+ * ```php
+ * $minus = curry(function ($x, $y) { return $x - $y; });
+ * $decrement = $minus(__(), 1);
+ * $decrement(10) // 9
+ *
+ * $reduce = curry('array_reduce');
+ * $sum = $reduce(__(), 'Tarsana\Functional\plus');
+ * $sum([1, 2, 3, 4], 0) // 10
+ * ```
  *
  * @signature * -> Placeholder
  * @return \Cypress\Curry\Placeholder
@@ -40,6 +57,11 @@ function _apply($fn, $args) {
 
 /**
  * Apply the provided function to the list of arguments.
+ * ```php
+ * apply('strlen', ['Hello']) // 5
+ * $replace = apply('str_replace');
+ * $replace(['l', 'o', 'Hello']) // 'Heooo'
+ * ```
  *
  * @signature (*... -> a) -> [*] -> a
  * @param  callable $fn
@@ -55,6 +77,13 @@ function apply() {
  * The leftmost function may have any arity;
  * the remaining functions must be unary.
  * The result of pipe is curried.
+ * **Calling pipe() without any argument throws Tarsana\Functional\Exceptions\InvalidArgument**
+ * ```php
+ * function add($x, $y) { return $x + $y; }
+ * $double = function($x) { return 2 * $x; };
+ * $addThenDouble = pipe('add', $double);
+ * $addThenDouble(2, 3) // 10
+ * ```
  *
  * @signature (((a, b, ...) -> o), (o -> p), ..., (y -> z)) -> ((a, b, ...) -> z)
  * @param  callable ...$fns
@@ -76,6 +105,11 @@ function pipe() {
 /**
  * A function that takes one argument and
  * returns exactly the given argument.
+ * ```php
+ * identity('Hello') // 'Hello'
+ * identity([1, 2, 3]) // [1, 2, 3]
+ * identity(null) // null
+ * ```
  *
  * @signature * -> *
  * @return callable
